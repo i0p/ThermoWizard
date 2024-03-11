72158c0574e6af0f59f6fea5d131296d79e5590e
iop <gnoom17@gmail.com>
2024-03-11 11:11:30 +0300

    добавлены свойства HumidAir

--- CoolPropsWraper.py
+++ CoolPropsWraper.py
@@ -4,6 +4,26 @@ import re
 from itertools import chain
 
 from scipy.constants import zero_Celsius
+
+
+def Pressure(altitude):
+    """
+        Altitude above sea level.
+
+        The pressure will be calculated by altitude above sea level according to
+        ASHRAE Fundamentals Handbook.
+
+        :param value: The value of the input [m].
+        :return: Altitude above sea level for the input.
+        :raises ValueError: If altitude above sea level is not between
+            -5000 and 11000 meters.
+        """
+    if not -5000 <= altitude <= 11000:
+        raise ValueError(
+            "Altitude above sea level should be between -5000 and 11000 meters!"
+            )
+    return 101325 * (1 - 2.25577e-5 * altitude) ** 5.2559
+
 class Temperature:
         """содержит температуру в кельвинах или градусах Цельсия"""
         class _Kelvin:
@@ -48,6 +68,52 @@ class HUAir:
         """Mass density per humid air unit [kg/m3]."""
         return 1 / self.Vha
 
+
+def pprint(humidair: HUAir): #именно этот
+    parameters_map = {'B' : 'B', 'Twb' : 'B', 'T_wb' : 'B', 'WetBulb ' : 'B',
+                      'C' : 'cp', 'cp ' : 'cp',
+                      'Cha' : 'Cha', 'cp_ha ' : 'Cha',
+                      'D' : 'D', 'Tdp' : 'D', 'DewPoint' : 'D', 'T_dp ' : 'D',
+                      'H' : 'H', 'Hda' : 'H', 'Enthalpy' : 'H',
+                      'Hha' : 'Hha',
+                      'K' : 'k', 'k' : 'k', 'Conductivity ' : 'k',
+                      'M' : 'mu', 'Visc' : 'mu', 'mu ' : 'mu',
+                      'psi_w' : 'Y', 'Y ' : 'Y',
+                      'P' : 'P',
+                      'P_w' : 'P_w',
+                      'R' : 'R', 'RH' : 'R', 'RelHum ' : 'R',
+                      'S' : 'S', 'Sda' : 'S', 'Entropy ' : 'S',
+                      'Sha ' : 'Sha',
+                      'T' : 'T', 'Tdb' : 'T', 'T_db ' : 'T',
+                      'V' : 'V', 'Vda ' : 'V',
+                      'Vha ' : 'Vha',
+                      'W' : 'W', 'Omega' : 'W', 'HumRat ' : 'W',
+                      'Z ' : 'Z',
+                      'density': 'density', 'Density': 'density'}
+
+    parameters_desc = {'B' : ('K', 'Wet-Bulb Temperature'),
+                       'cp' : ('J/kg dry air/K', 'Mixture specific heat per unit dry air'),
+                       'Cha' : ('J/kg humid air/K', 'Mixture specific heat per unit humid air'),
+                       'D' : ('K', 'Dew-Point Temperature'),
+                       'H' : ('J/kg dry air', 'Mixture enthalpy per dry air'),
+                       'Hha' : ('J/kg humid air', 'Mixture enthalpy per humid air'),
+                       'k' : ('W/m/K', 'Mixture thermal conductivity'),
+                       'mu' : ('Pa-s', 'Mixture viscosity'),
+                       'Y' : ('mol water/mol humid air', 'Water mole fraction'),
+                       'P' : ('Pa', 'Pressure'),
+                       'P_w' : ('Pa', 'Partial pressure of water vapor'),
+                       'R' : ('', 'Relative humidity in [0, 1]'),
+                       'S' : ('J/kg dry air/K ', 'Mixture entropy per unit dry air'),
+                       'Sha' : ('J/kg humid air/K ', 'Mixture entropy per unit humid air'),
+                       'T' : ('K', 'Dry-Bulb Temperature'),
+                       'V' : ('m³/kg dry air', 'Mixture volume per unit dry air'),
+                       'Vha' : ('m³/kg humid air', 'Mixture volume per unit humid air'),
+                       'density': ('kg/m³ humid air', "Mass density per humid air unit"),
+                       'W' : ('kg water/kg dry air', 'Humidity Ratio'),
+                       'Z' : ('', 'Compressibility factor (Z=pv/(RT))')
+                       }
+    data = [ (parameters_desc[parameters_map[param]][1], parameters_desc[parameters_map[param]][0], getattr(humidair, param) ) for param in ("P", "T", "R", "Twb", "Hda", "Hha", "W", "DewPoint", "density", "P_w")]
+    print(tabulate(data, tablefmt="plain"))
 
 class mainProps:
         props = {
